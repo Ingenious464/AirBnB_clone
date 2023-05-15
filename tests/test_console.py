6b19be1cce4ea7893e414e0c9a910cc3ac6b49ca
AyanSwarga <ayannicholas@gmail.com>
2023-05-15 22:52:51 +0100

    updated

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""This script defines unittests for console.py.
-
+"""Defines unittests for console.py.
 Unittest classes:
     TestHBNBCommand_prompting
     TestHBNBCommand_help
